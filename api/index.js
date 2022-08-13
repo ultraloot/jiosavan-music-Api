33d7127b9468cba12bba437808f9eb74cd00ba64
Aryan Raz <109226533+Coderaryan-offical@users.noreply.github.com>
2022-08-13 12:00:03 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const package = require("../package.json");
 module.exports = async (req, res) => {
   res.json({
     alive: true,
-    made_by: "https://github.com/tuhinpal",
+    made_by: "https://github.com/Coderaryan-offical",
     version: package.version,
     docs: `${process.env.APP_URL || "http://localhost:3000"}/docs`,
   });
